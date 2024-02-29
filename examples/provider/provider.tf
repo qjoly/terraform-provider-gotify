cbf0d51d41dfc3f9dda58ed78d4f61bb986337e7
Quentin JOLY <github@thoughtless.eu>
2024-02-29 09:48:31 +0100

    Update example provider

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,14 @@
-provider "scaffolding" {
-  # example configuration here
+
+terraform {
+  required_providers {
+    gotify = {
+      source  = "terraform.local/local/gotify"
+      version = "0.0.1"
+    }
+  }
+}
+
+provider "gotify" {
+  token = "CAZMEZi72TLmRCE"
+  url = "http://localhost:8080"
 }
